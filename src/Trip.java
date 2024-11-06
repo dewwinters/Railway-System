9ddc6e5cc95fff3488e60e44cb91a73c5ac94705
Dewnitrous <ITCSIU22017@student.hcmiu.edu.vn>
2024-11-06 14:07:26 +0700

    Modified some basic variables for classes

--- src/Trip.java
+++ src/Trip.java
@@ -1,8 +1,8 @@
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Trip {
-
     private int id;
     private String start;
     private String destination;
@@ -11,4 +11,9 @@ public class Trip {
     private LocalDate date;
     private int bookedSeats;
     private double price;
+    private Employee driver;
+    private Train train;
+    private ArrayList<Passenger> passengers;
+
+    
 }
