cc81221a520be3dd904e03e1e0bb741ed6e04218
Dewnitrous <ITCSIU22017@student.hcmiu.edu.vn>
2024-11-06 13:28:18 +0700

    Init Main, Train and Trip class

---
+++ src/Trip.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Trip {
+
+    private int id;
+    private String start;
+    private String destination;
+    private LocalTime departureTime;
+    private LocalTime arrivalTime;
+    private LocalDate date;
+    private int bookedSeats;
+    private double price;
+}
